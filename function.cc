4fbe867f209bee41c6673c6d507f51c6d3154c33
Samuel E Brinck (US) <samuel.e.brinck@lmco.com>
2019-05-02 09:53:24 -0600

    removed tabs

--- function.cc
+++ function.cc
@@ -11,22 +11,22 @@ bar* findToken(const std::string hugeString)
     unsigned char token;
     char* hugeCopy;
     bar retval;
-	
-	if(hugeString.size() > 1000)
-	{
-	    std::cout << "Using token '/'\n";
-	    token = '/';
-	}
-	else
-	    std::cout << "Using token '-'\n";
-		token = '-';
+    
+    if(hugeString.size() > 1000)
+    {
+        std::cout << "Using token '/'\n";
+        token = '/';
+    }
+    else
+        std::cout << "Using token '-'\n";
+        token = '-';
   
     hugeCopy = new char[398328];
     strcpy(hugeCopy, hugeString->c_str());
   
     // Find the first index of the token within our string by calling our
-	// function findFirst(char* inputString, int inputStringSize, char token);
-	// If it cannot find the token it returns 0.
+    // function findFirst(char* inputString, int inputStringSize, char token);
+    // If it cannot find the token it returns 0.
     location = findFirst(hugeCopy, 398328, token);
     if( location = 0 )
     {
@@ -34,10 +34,10 @@ bar* findToken(const std::string hugeString)
         return nullptr;
     }
     else
-	{
+    {
         retval.location = location;
-		retval.token = token;
-	}
+        retval.token = token;
+    }
     delete hugeCopy;
     return &retval;
 }
